ae74035b9fb608db4bc2b3cf1214fb57ba03a675
轻秋 <Lachuann@outlook.com>
2024-09-23 11:42:35 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/🎥影音娱乐/👨‍🎓哲理','/🏫教程详解','/🗄️资源管理/源码类'],
+  protectedRoutes: ['/🎥影音娱乐/👨‍🎓哲理','/🏫教程详解','/🗄️资源管理/源码类','/📌图床/Zero','/🗄️资源管理/NULL'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'yingchuan027@gmail.com',
